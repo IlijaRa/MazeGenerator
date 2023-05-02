365c98755c25cb31434bb65941f530ed6b385b35
IlijaRa <rakocevicilija7@gmail.com>
2023-05-02 15:32:43 +0200

    wilsons algorithm works properly and added algorithm button triggers

--- js/maze_generators/wilson's.js
+++ js/maze_generators/wilson's.js
@@ -1,35 +1,55 @@
+let encounteredVisitedCell = new Cell();
+
 function wilsons(){
     // CURRENT_CELL is set to visited so grid has one visited cell which enables algorithm to execute in a good manner
 
     let loopErasedCells = loopErasedRandomWalk();
+    
+    if(loopErasedCells.length < 1)
+        return;
 
     // setting cells to visited in loopErasedCells array
     loopErasedCells.forEach(cell => {
         cell.visited = true;
+        cell.highlight();
     });
 
     // removing walls between cells in loopErasedCells array
     for(i = 0; i < loopErasedCells.length; i++){
-        if(i + 1 < loopErasedCells.length)
+        if(loopErasedCells[i] != loopErasedCells[loopErasedCells.length - 1])
             removeWall(loopErasedCells[i], loopErasedCells[i + 1]);
+        else if(loopErasedCells[i] == loopErasedCells[loopErasedCells.length - 1])
+            removeWall(loopErasedCells[i], encounteredVisitedCell);
     }
 
+    encounteredVisitedCell = new Cell();
+    
     loopErasedCells = [];
 }
 
 function loopErasedRandomWalk(){
     let loopErasedCells = [];
 
-    let currentCell = getRandomUnvisitedCell();
+    let result = getRandomUnvisitedCell();
 
-    while(true){
+    let currentCell = result[0];
+
+    while(result[1] > 0){
         let neighbors = getNeighborsIncludingVisitedNWES(currentCell);
 
         let randomNumber = floor(random(0, neighbors.length));
     
         let neighbor = neighbors[randomNumber];
 
+        // if all neighbors are visited, mark current cell as visited and join with random neighbor
+        if(CheckIfAllNeighborsAreVisited(neighbors)){
+            currentCell.visited = true;
+            removeWall(currentCell, neighbor);
+            break;
+        }
+
         if(neighbor.visited == true){
+            encounteredVisitedCell = neighbor;
             break;
         }
 
@@ -60,5 +80,19 @@ function getRandomUnvisitedCell(){
 
     let randomNumber = floor(random(0, totalUnvisitedCells.length));
 
-    return totalUnvisitedCells[randomNumber];
+    return [totalUnvisitedCells[randomNumber], totalUnvisitedCells.length];
+}
+
+function CheckIfAllNeighborsAreVisited(neighbors){
+    let visitedCounter = 0;
+    
+    neighbors.forEach(cell => {
+        if(cell.visited == true)
+            visitedCounter++;
+    });
+
+    if(visitedCounter == neighbors.length)// if all neighbors are visited
+        return true;
+    else
+        return false;
 }
--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,3 @@
-// document.querySelector('.navbar-toggle').addEventListener('click', function() {
-//     document.querySelector('.navbar-nav').classList.toggle('active');
-// });
-
-// document.querySelectorAll('.nav-item.dropdown').forEach(function(item) {
-//     item.addEventListener('click', function() {
-//         this.querySelector('.dropdown-menu').classList.toggle('active');
-//     });
-// });
-
-// document.getElementById('breadth-first-search-btn').addEventListener('click', function(e){
-//     e.target
-// });
+document.getElementById('binary-tree-btn').addEventListener('click', function(e){
+    BINARY_TREE_BUTTON = 1;
+});
